ff36aaaff8efbaf722c999ad51155271dbe6c57e
Jayashri Patil <varshap3603@gmail.com>
2026-05-05 13:45:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,3 @@
-import subprocess, sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly", "pandas", "-q"])
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
